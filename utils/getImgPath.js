79fab88d437d8927f615783b9c793be1f33c4056
Darnell <darnjames@gmail.com>
2024-06-25 19:47:33 -0400

    Remove port from image path

--- utils/getImgPath.js
+++ utils/getImgPath.js
@@ -1,7 +1,3 @@
-require("dotenv").config();
-
-const port = process.env.PORT;
-
 exports.getImgPath = (req, imgName) => {
-  return `${req.protocol}://${req.hostname}:${port}/${imgName}`;
+  return `${req.protocol}://${req.hostname}/${imgName}`;
 };
